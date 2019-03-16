8defc1fe1ff28a265e00e9f989a6d471d560723c
Mokeas <37297059+Mokeas@users.noreply.github.com>
2019-03-16 15:46:18 +0100

    Update raf.sh

--- raf.sh
+++ raf.sh
@@ -1,1 +1,3 @@
-List ONLY files(not directories) in actual directory, which have '.' (but are not hidden) in the name and do not contain capital letters: A,B, ..., save the list in a file 'nonCapital' and write how many of them there is on standart output
+List ONLY files(not directories) in current directory, which have '.' (but are not hidden) in the name and 
+do not contain capital letters: A,B, ..., save the list in a file 'nonCapital' and write how many of them there is on standart output
+Use one command with pipes, not multiple separate commands!
